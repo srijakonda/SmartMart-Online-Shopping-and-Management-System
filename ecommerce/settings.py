130441fe858bf8773a7bc6a1ec9f669586d9b1a0
srija <srijakonda37@gmail.com>
2026-05-11 10:03:34 +0530

    Added static root

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
